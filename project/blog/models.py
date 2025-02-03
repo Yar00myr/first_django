dc40898c7ecf12e5a3c0502b6977af0f8d56d6ce
Yar00myr <ivanucaromir@gmail.com>
2025-02-03 21:05:47 +0000

    done homework for second lesson add models for blog(post and comment)

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=25, unique=True)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return f"{self.title}"
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+
+    def __repr__(self):
+        return f"{self.content}"
