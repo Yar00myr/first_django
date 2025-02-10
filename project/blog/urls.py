8150dd1abd17e2ddf1c52b01abdcd123c3e38566
Yar00myr <ivanucaromir@gmail.com>
2025-02-10 20:43:39 +0000

    done hm 4 (form)

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import first_django, one_post
+from .views import home, one_post, create_post
 
 
 app_name = "blog"
 
 urlpatterns = [
-    path("first_start/", first_django, name="first"),
+    path("home/", home, name="home"),
     path("one_post/<int:post_id>/", one_post, name="one_post"),
+    path("create_post/", create_post, name="create_post"),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
+from django.contrib.auth.models import User
 from .models import Post, Comment
 
 
-def first_django(request):
+def home(request):
     posts = Post.objects.all()
     data = {"posts": posts}
     return render(request, "index.html", context=data)
@@ -12,3 +13,19 @@ def one_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     data = {"post": post}
     return render(request, "post.html", context=data)
+
+
+def create_post(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        author = get_object_or_404(User)
+        try:
+            post = Post(title=title, content=content, author=author)
+            post.full_clean()
+        except:
+            return HttpResponse("<h1>Bad values</h1>")
+        else:
+            post.save()
+        return redirect("blog:home")
+    return render(request, "create_post.html")
